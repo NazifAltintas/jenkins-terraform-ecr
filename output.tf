f35d44777135202975e2c5b6ec1500f495525b9a
aviator12 <91530823+NazifAltintas@users.noreply.github.com>
2024-03-01 01:41:38 +0100

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
 output "registry_id" {
   description = "The account ID of the registry holding the repository."
-  value = aws_ecr_repository.devopsyolujenkins.registry_id
+  value = aws_ecr_repository.demoaviatorjenkins.registry_id
 }
 
 output "repository_url" {
   description = "The account ID of the registry holding the repository."
-  value = aws_ecr_repository.devopsyolujenkins.repository_url
+  value = aws_ecr_repository.demoaviatorjenkins.repository_url
 }
