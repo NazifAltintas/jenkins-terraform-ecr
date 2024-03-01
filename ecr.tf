00c01911e6cdd34ec7a24e1cf742ce17bd08625a
aviator12 <91530823+NazifAltintas@users.noreply.github.com>
2024-03-01 01:40:58 +0100

    Update ecr.tf

--- ecr.tf
+++ ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "demoaviatorjenkins" {
-  name = "devopsyolujenkins"
+  name = "demoaviatorjenkins"
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = true
